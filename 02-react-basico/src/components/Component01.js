88294b1aafa16b96d08e58b1aafafae2af24c45e
Edgar Bisset <ebicet@gmail.com>
2023-08-17 15:30:35 -0300

    atualizado

---
+++ 02-react-basico/src/components/Component01.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Component01 = () => {
+  return (
+    <div>
+      Component01
+      <h1> esse é o meu primeiro teste sem ajuda.</h1>
+    </div>
+  );
+};
+
+export default Component01;
